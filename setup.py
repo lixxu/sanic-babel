e028694bbdf09c8f86ca5b21ce372b412043719b
lixxu <xuzenglin@gmail.com>
2017-03-09 23:51:53 +0800

    update version to update on pypi

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='sanic-babel',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/lixxu/sanic-babel',
     license='BSD',
     author='Lix Xu',
